6bb521b7cea84f14310439c3a22f38fba8ab9463
penguinsan <penguinsan.pg@gmail.com>
2024-02-17 17:22:19 +0900

    VStack の alignment を設定

--- App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
+++ App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
                 .font(.title)
             Text("Joshua Tree National Park")
