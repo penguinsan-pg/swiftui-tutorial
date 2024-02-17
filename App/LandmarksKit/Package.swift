89ad94bbd49b6060323f64d7d993922089bbc992
penguinsan <penguinsan.pg@gmail.com>
2024-02-17 16:05:00 +0900

    Swift Package を作成

---
+++ App/LandmarksKit/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LandmarksKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LandmarksKit",
+            targets: ["LandmarksKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LandmarksKit"),
+        .testTarget(
+            name: "LandmarksKitTests",
+            dependencies: ["LandmarksKit"]),
+    ]
+)
