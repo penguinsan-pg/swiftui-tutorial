1d07abea9bcdb7bc9d5a302ddb2ea58d02fcfbbf
penguinsan <penguinsan.pg@gmail.com>
2024-02-18 16:32:10 +0900

    地図に表示する座標を指定するプロパティを追加

--- App/LandmarksKit/Sources/LandmarksKit/MapView.swift
+++ App/LandmarksKit/Sources/LandmarksKit/MapView.swift
@@ -11,6 +11,13 @@ import SwiftUI
 
 struct MapView: View {
 
+    private var region: MKCoordinateRegion {
+        .init(
+            center: .init(latitude: 34.011_286, longitude: -116.166_868),
+            span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+
     var body: some View {
         Text("Hello, World!")
     }
