ec0109911eb961ade43d35cea8186296bc93e993
penguinsan <penguinsan.pg@gmail.com>
2024-02-18 16:38:56 +0900

    現在の ContentView の body の中身を VStack で囲む

--- App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
+++ App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
@@ -11,18 +11,20 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
