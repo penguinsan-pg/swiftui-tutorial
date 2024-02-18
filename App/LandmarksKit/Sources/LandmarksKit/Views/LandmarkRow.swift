1e460f0dc4831cc73b3013a1f7c57514dc55dd92
penguinsan <penguinsan.pg@gmail.com>
2024-02-18 17:53:14 +0900

    LandmarkRow を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  
+//  
+//  Created by penguinsan on 2024/02/18
+//  
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
