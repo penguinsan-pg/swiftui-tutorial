616a3968cbd13fd34efa0cc31dc40b3a43738945
penguinsan <penguinsan.pg@gmail.com>
2024-02-18 16:26:58 +0900

    MapView を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  
+//  
+//  Created by penguinsan on 2024/02/18
+//  
+//
+
+import SwiftUI
+
+struct MapView: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MapView()
+}
