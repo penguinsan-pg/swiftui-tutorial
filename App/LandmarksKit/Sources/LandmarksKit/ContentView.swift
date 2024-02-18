06dc32c2c2584389d932f5518b4b9ed2439d21ad
penguinsan <penguinsan.pg@gmail.com>
2024-02-18 16:51:11 +0900

    小見出しを表示する Text の Modifier は HStack に対して設定するように修正

--- App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
+++ App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
@@ -24,11 +24,11 @@ struct ContentView: View {
                     .font(.title)
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                     Spacer()
                     Text("California")
-                        .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
 
                 Divider()
 
