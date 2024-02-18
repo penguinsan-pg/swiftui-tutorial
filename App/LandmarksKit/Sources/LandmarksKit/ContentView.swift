ff777d68f1869e8491930ccee946aea6683d727f
penguinsan <penguinsan.pg@gmail.com>
2024-02-18 16:41:41 +0900

    ContentView に CircleImage を追加

--- App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
+++ App/LandmarksKit/Sources/LandmarksKit/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
             MapView()
                 .frame(height: 300)
 
+            CircleImage()
+
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
