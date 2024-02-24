c0f3a0865b88b0c89cc9df7b4945e7a10de11a00
penguinsan <penguinsan.pg@gmail.com>
2024-02-24 16:34:03 +0900

    LandmarkList を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  
+//  
+//  Created by penguinsan on 2024/02/24
+//  
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
