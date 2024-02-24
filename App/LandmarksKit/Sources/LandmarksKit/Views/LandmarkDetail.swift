5c8c54f08db0605fde455ca2ff244e49ee81a55b
penguinsan <penguinsan.pg@gmail.com>
2024-02-24 16:48:49 +0900

    LandmarkDetail を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  
+//  
+//  Created by penguinsan on 2024/02/24
+//  
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
