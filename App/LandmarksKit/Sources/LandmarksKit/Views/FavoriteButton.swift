1a91b8f36b7ae1fc538b12c16c12f3840f60f692
penguinsan <penguinsan.pg@gmail.com>
2024-02-25 13:31:43 +0900

    6-1: FavoriteButton を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  
+//  
+//  Created by penguinsan on 2024/02/25
+//  
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
