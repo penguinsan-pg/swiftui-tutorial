e21f692a2dc2272241cfde32d7601f0c3701b336
penguinsan <penguinsan.pg@gmail.com>
2024-02-25 12:58:46 +0900

    3-1: List の内部に ForEach を追加
    - List で複数種類の View を動的に表示したい場合は ForEach を利用する

--- App/LandmarksKit/Sources/LandmarksKit/Views/LandmarkList.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/LandmarkList.swift
@@ -20,11 +20,13 @@ struct LandmarkList: View {
 
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
