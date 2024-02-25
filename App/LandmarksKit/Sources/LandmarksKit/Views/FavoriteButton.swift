3bcd909ca9bf24adc27f2f43e9bcc9ad69f6568c
penguinsan <penguinsan.pg@gmail.com>
2024-02-25 13:38:17 +0900

    6-3: FavoriteButton の body を実装

--- App/LandmarksKit/Sources/LandmarksKit/Views/FavoriteButton.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/FavoriteButton.swift
@@ -13,7 +13,13 @@ struct FavoriteButton: View {
     @Binding var isSet: Bool
 
     var body: some View {
-        Text("Hello, World!")
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
     }
 }
 
