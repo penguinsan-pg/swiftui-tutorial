bacd66365a4e164092db64a4dbe9481af07fa926
penguinsan <penguinsan.pg@gmail.com>
2024-02-28 22:49:12 +0900

    1-6: 六角形の各辺のデータを追加

--- App/LandmarksKit/Sources/LandmarksKit/Views/HexagonParameters.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/HexagonParameters.swift
@@ -17,5 +17,35 @@ struct HexagonParameters {
     }
 
     static let segments = [
+        Segment(
+            line: .init(x: 0.60, y: 0.05),
+            curve: .init(x: 0.40, y: 0.05),
+            control: .init(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line: .init(x: 0.05, y: 0.20),
+            curve: .init(x: 0.00, y: 0.30),
+            control: .init(x: 0.00, y: 0.25)
+        ),
+        Segment(
+            line: .init(x: 0.00, y: 0.70),
+            curve: .init(x: 0.05, y: 0.80),
+            control: .init(x: 0.00, y: 0.75)
+        ),
+        Segment(
+            line: .init(x: 0.40, y: 0.95),
+            curve: .init(x: 0.60, y: 0.95),
+            control: .init(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line: .init(x: 0.95, y: 0.80),
+            curve: .init(x: 1.00, y: 0.70),
+            control: .init(x: 1.00, y: 0.75)
+        ),
+        Segment(
+            line: .init(x: 1.00, y: 0.30),
+            curve: .init(x: 0.95, y: 0.20),
+            control: .init(x: 1.00, y: 0.25)
+        ),
     ]
 }
