2b9766f7efcaa4d9efd0138957a564f84575989d
penguinsan <penguinsan.pg@gmail.com>
2024-02-28 22:56:28 +0900

    2-1: BadgeBackground を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/BadgeBackground.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeBackground.swift
+//  
+//  
+//  Created by penguinsan on 2024/02/28
+//  
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
