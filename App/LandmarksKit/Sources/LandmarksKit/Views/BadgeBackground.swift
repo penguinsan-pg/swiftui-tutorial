63d7e8c2f0d49ed9b4cfe4bc02a0ee0ffe6e6ccc
penguinsan <penguinsan.pg@gmail.com>
2024-02-28 23:46:06 +0900

    2-6: Path を GeometryReader で囲い、描画領域を View に合わせて変更できるようにする
    - width のハードコーディングがなくなる

--- App/LandmarksKit/Sources/LandmarksKit/Views/BadgeBackground.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/BadgeBackground.swift
@@ -11,37 +11,39 @@ import SwiftUI
 struct BadgeBackground: View {
 
     var body: some View {
-        Path { path in
-            var width: CGFloat = 100.0
-            let height = width
-            path.move(
-                to: .init(
-                    x: width * 0.95,
-                    y: height * (0.20 + HexagonParameters.adjustment)
-                )
-            )
-
-            HexagonParameters.segments.forEach { segment in
-                path.addLine(
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                path.move(
                     to: .init(
-                        x: width * segment.line.x,
-                        y: height * segment.line.y
+                        x: width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
 
-                path.addQuadCurve(
-                    to: .init(
-                        x: width * segment.curve.x,
-                        y: height * segment.curve.y
-                    ),
-                    control: .init(
-                        x: width * segment.control.x,
-                        y: height * segment.control.y
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: .init(
+                            x: width * segment.line.x,
+                            y: height * segment.line.y
+                        )
                     )
-                )
+
+                    path.addQuadCurve(
+                        to: .init(
+                            x: width * segment.curve.x,
+                            y: height * segment.curve.y
+                        ),
+                        control: .init(
+                            x: width * segment.control.x,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
             }
+            .fill(.black)
         }
-        .fill(.black)
     }
 }
 
