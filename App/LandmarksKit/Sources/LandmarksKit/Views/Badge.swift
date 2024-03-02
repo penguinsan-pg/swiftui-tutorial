a06012b0845b9d0b1d08e0860b7b70cbfb999473
penguinsan <penguinsan.pg@gmail.com>
2024-03-02 16:50:24 +0900

    4-1: Badge を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  
+//  
+//  Created by penguinsan on 2024/03/02
+//  
+//
+
+import SwiftUI
+
+struct Badge: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Badge()
+}
