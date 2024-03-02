6b5b252e8bf53b76b328bc0d6b3818ae6949df28
penguinsan <penguinsan.pg@gmail.com>
2024-03-02 16:32:38 +0900

    3-3: 山のシンボルの上部分を描画

--- App/LandmarksKit/Sources/LandmarksKit/Views/BadgeSymbol.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/BadgeSymbol.swift
@@ -11,7 +11,24 @@ import SwiftUI
 struct BadgeSymbol: View {
 
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width * 0.5
+                let topWidth = width * 0.226
+                let topHeight = width * 0.488
+
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing),
+                ])
+            }
+        }
     }
 }
 
