3bba05add9fdff015a9c9734cf9afbc5c2b19c3b
penguinsan <penguinsan.pg@gmail.com>
2024-03-17 17:47:12 +0900

    3-1: CategoryRow を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  
+//  
+//  Created by penguinsan on 2024/03/17
+//  
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
