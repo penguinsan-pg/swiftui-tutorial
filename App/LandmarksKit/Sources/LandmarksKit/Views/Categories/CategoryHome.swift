254fc17dc19b9d12a79d466d52e8db7b11db260a
penguinsan <penguinsan.pg@gmail.com>
2024-03-17 18:18:04 +0900

    4-4: List の先頭に、features の先頭の要素の画像を表示する

--- App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryHome.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryHome.swift
@@ -15,6 +15,12 @@ struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
