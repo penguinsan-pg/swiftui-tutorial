8232b24ab6991315be20ee767bb2ceb757c2c0b8
penguinsan <penguinsan.pg@gmail.com>
2024-03-17 17:26:26 +0900

    1-3: NavigationBar のタイトルを設定

--- App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryHome.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryHome.swift
@@ -13,6 +13,7 @@ struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
             Text("Hello, World!")
+                .navigationTitle("Featured")
         } detail: {
             Text("Select a Landmark")
         }
