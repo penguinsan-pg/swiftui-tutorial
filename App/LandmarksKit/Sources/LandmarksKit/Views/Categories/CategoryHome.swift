66c2b3d0cc6c8faf875352e921ed42369c9b3cf5
penguinsan <penguinsan.pg@gmail.com>
2024-03-17 17:22:27 +0900

    1-1: CategoryHome を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  
+//  
+//  Created by penguinsan on 2024/03/17
+//  
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
