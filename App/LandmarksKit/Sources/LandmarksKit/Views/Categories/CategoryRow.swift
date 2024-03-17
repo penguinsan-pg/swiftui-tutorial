cef6f005f96aed2e883e38fa0ffd57625be09f0a
penguinsan <penguinsan.pg@gmail.com>
2024-03-17 17:50:53 +0900

    3-2: CategoryRow にカテゴリー名とランドマーク一覧のプロパティを追加

--- App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryRow.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Categories/CategoryRow.swift
@@ -10,11 +10,18 @@ import SwiftUI
 
 struct CategoryRow: View {
 
+    var categoryName: String
+    var items: [Landmark]
+
     var body: some View {
         Text("Hello, World!")
     }
 }
 
 #Preview {
-    CategoryRow()
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(3))
+    )
 }
