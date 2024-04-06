31d8c226bbc37d85d53209217aee958a212da33a
penguinsan <penguinsan.pg@gmail.com>
2024-04-06 19:06:35 +0900

    3-1: ProfileEditor を作成

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileEditor.swift
+//  
+//  
+//  Created by penguinsan on 2024/04/06
+//  
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+
+    @Binding var profile: Profile
+
+    var body: some View {
+        List {
+            HStack {
+                Text("Username")
+                Spacer()
+                TextField("Username", text: $profile.username)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.trailing)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileEditor(profile: .constant(.default))
+}
