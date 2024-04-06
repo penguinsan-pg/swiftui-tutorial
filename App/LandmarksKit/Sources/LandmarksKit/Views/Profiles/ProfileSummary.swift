696783224afe1f6b38096a8e5bc0c6a94d870ec8
penguinsan <penguinsan.pg@gmail.com>
2024-04-06 17:52:10 +0900

    1-3: ProfileSummary を作成
    - 親View (ProfileHost) が profile の状態を管理するため、ここでは @Binding にしなくて良い

---
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileSummary.swift
+//  
+//  
+//  Created by penguinsan on 2024/04/06
+//  
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+
+    var profile: Profile
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+
+                Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileSummary(profile: .default)
+}
