ce21b1e3040c2644b2d0fba4eb212f1665aa5e4d
penguinsan <penguinsan.pg@gmail.com>
2024-04-06 19:26:05 +0900

    3-5: ランドマーク訪問のゴール日を設定する DatePicker を追加

--- App/LandmarksKit/Sources/LandmarksKit/Views/Profiles/ProfileEditor.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/Profiles/ProfileEditor.swift
@@ -12,6 +12,12 @@ struct ProfileEditor: View {
 
     @Binding var profile: Profile
 
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+
     var body: some View {
         List {
             HStack {
@@ -30,6 +36,10 @@ struct ProfileEditor: View {
                 ForEach(Profile.Season.allCases) { season in
                     Text(season.rawValue).tag(season)
                 }
+            }
+
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date")
             }
         }
     }
