581aa4dfe430e31549c643fdbe46f01ff628bf40
penguinsan <penguinsan.pg@gmail.com>
2024-04-07 17:30:35 +0900

    1-2: `makeUIViewController(context:)` を実装
    - SwiftUI によって View が生成されるタイミングで、一度だけ呼び出される

--- App/LandmarksKit/Sources/LandmarksKit/Views/PageView/PageViewController.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/PageView/PageViewController.swift
@@ -12,4 +12,13 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
 
     var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+
+        return pageViewController
+    }
 }
