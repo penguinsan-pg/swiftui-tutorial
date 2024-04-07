be6127bd5aba283ab6948b7c26835a8d19fb3543
penguinsan <penguinsan.pg@gmail.com>
2024-04-07 19:20:47 +0900

    4-3: PageControl の Coordinator を作成

--- App/LandmarksKit/Sources/LandmarksKit/Views/PageView/PageControl.swift
+++ App/LandmarksKit/Sources/LandmarksKit/Views/PageView/PageControl.swift
@@ -11,8 +11,24 @@ import UIKit
 
 struct PageControl: UIViewRepresentable {
 
+    class Coordinator: NSObject {
+        var control: PageControl
+
+        init(_ control: PageControl) {
+            self.control = control
+        }
+
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+
     var numberOfPages: Int
     @Binding var currentPage: Int
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
